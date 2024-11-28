16e7c848c377deb6bd4be1b4f3057df36712ed2d
Ícaro Alencar Sousa <ias_epf@hotmail.com>
2024-11-28 17:01:34 -0300

    Criação do projeto, primeiros componentes e regras de negócio.

---
+++ apps/frontend/src/components/evento/AcessarViaQrCode.tsx
@@ -0,0 +1,27 @@
+<<<<<<< HEAD
+import { Evento } from "core";
+=======
+import { Evento } from "@/core";
+>>>>>>> e1d2c3fdb8db39e2b06011915fc1a45802b01a3d
+import QRCode from "react-qr-code";
+
+export interface AcessarViaQrCodeProps {
+  evento: Evento;
+}
+
+export default function AcessarViaQrCode(props: AcessarViaQrCodeProps) {
+  return (
+    <div className="flex flex-col items-center justify-center gap-4 border border-zinc-800 px-10">
+      <span className="text-sm font-light text-zinc-400">
+        Acesse via Mobile
+      </span>
+      <QRCode
+        value={JSON.stringify({
+          id: props.evento.id,
+          senha: props.evento.senha,
+        })}
+        className="w-32 h-32"
+      />
+    </div>
+  );
+}
---
+++ apps/frontend/src/components/evento/ListaConvidados.tsx
@@ -0,0 +1,22 @@
+<<<<<<< HEAD
+import { Convidado } from "core";
+=======
+import { Convidado } from "@/core";
+>>>>>>> e1d2c3fdb8db39e2b06011915fc1a45802b01a3d
+import ConvidadoItem from "./ConvidadoItem";
+
+export interface ListaConvidadosProps {
+  convidados: Convidado[];
+}
+
+export default function ListaConvidados(props: ListaConvidadosProps) {
+  return (
+    <div>
+      <ul className="flex flex-col gap-2">
+        {props.convidados.map((convidado) => (
+          <ConvidadoItem key={convidado.id} convidado={convidado} />
+        ))}
+      </ul>
+    </div>
+  );
+}
